20ea6c23bfbf6c6befb267d507bbd1dceb71e9f5
Anastasiia Fursyk <anastasianauk44@gmail.com>
2024-11-27 15:23:06 +0200

    Changed styles in detail meal screen

--- app/src/main/java/com/example/cookup/ui/elements/MealDetailScreen.kt
+++ app/src/main/java/com/example/cookup/ui/elements/MealDetailScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavHostController
 import coil.compose.rememberAsyncImagePainter
@@ -104,13 +105,32 @@ fun DisplayMealDetails(
             Text(text = if (isFavorite.value) "Remove from Favorites" else "Add to Favorites", style = MaterialTheme.typography.bodySmall)
         }
         Spacer(modifier = Modifier.height(8.dp))
-        Text(
-            text = "Category: ${meal.strCategory}",
-            style = MaterialTheme.typography.bodyMedium
-        )
-        Text(text = "Region: ${meal.strArea}", style = MaterialTheme.typography.bodyMedium)
+        Row(modifier = Modifier.padding(bottom = 6.dp)) {
+            Text(
+                text = "Category:",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(end = 6.dp),
+                color = Color(0xFFFD5D69)
+            )
+            Text(
+                text = "${meal.strCategory}",
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+        Row() {
+            Text(
+                text = "Region:",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(end = 6.dp),
+                color = Color(0xFFFD5D69)
+            )
+            Text(
+                text = "${meal.strArea}",
+                style = MaterialTheme.typography.bodyMedium,
+            )
+        }
         Spacer(modifier = Modifier.height(8.dp))
-        Text(text = "Ingredients:", style = MaterialTheme.typography.bodyMedium)
+        Text(text = "Ingredients:", style = MaterialTheme.typography.bodyLarge, color = Color(0xFFFD5D69), modifier = Modifier.padding(bottom = 8.dp))
         // Відображення інгредієнтів і їх кількості
         meal.ingredients.zip(meal.measures).forEach { (ingredient, measure) ->
             if (ingredient.isNotEmpty() && measure.isNotEmpty()) {
@@ -121,8 +141,7 @@ fun DisplayMealDetails(
             }
         }
         Spacer(modifier = Modifier.height(8.dp))
-        Text(text = "Instructions:", style = MaterialTheme.typography.bodyMedium)
-        Text(text = meal.strInstructions, style = MaterialTheme.typography.bodySmall)
-
+        Text(text = "Instructions:", style = MaterialTheme.typography.bodyLarge, color = Color(0xFFFD5D69), modifier = Modifier.padding(bottom = 8.dp))
+        Text(text = meal.strInstructions, style = MaterialTheme.typography.bodySmall, modifier = Modifier.padding(bottom = 8.dp))
     }
 }
